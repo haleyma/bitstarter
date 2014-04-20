5d5c2ad31dd24f432a3cf668f4635e9fcdd5d786
Ubuntu <ubuntu@domU-12-31-39-15-21-62.compute-1.internal>
2014-04-20 02:55:04 +0000

    update index.js to read from index.html

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!')
-})
+  response.send(fs.readFileSync(index.html, [String])
+)})
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))

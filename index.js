9cff473ee3cfd8c7f41c537a573f5d62a5f708a0
Ubuntu <ubuntu@domU-12-31-39-15-21-62.compute-1.internal>
2014-04-20 03:28:06 +0000

    Another index.js update

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var express = require('express')
 var app = express();
 var fs = require('fs');
-var mybuf = fs.readFileSync("index.html")
+var mybuf = fs.readFileSync("index.html");
 
-));
+
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
